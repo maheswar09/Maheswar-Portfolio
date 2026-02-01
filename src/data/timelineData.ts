ed253ffd9c7260bd707df733a1cada328de05b65
maheswar09 <maheswargorantla9@gmail.com>
2026-02-01 13:08:02 -0500

    updated description

--- src/data/timelineData.ts
+++ src/data/timelineData.ts
@@ -21,10 +21,10 @@ export const timelineData: TimelineItem[] = [
     duration: 'Jul 2025 – Present',
     current: true,
     description: [
-      "Maintained and enhanced Node.js and Express.js microservices for a school management system supporting 3000+ parents and staff, handling enrollment, billing, messaging, and analytics modules.",
-      "Built secure and scalable REST APIs using JWT-based authentication, role-based access control (RBAC), and middleware-driven authorization, adhering to OWASP security standards.",
-      "Implemented CI/CD pipelines with GitHub Actions, Docker, AWS ECR and AWS ECS (blue-green deployments) reducing deployment time by 25% and improving release reliability.",
-      "Improved system reliability and observability by integrating AWS CloudWatch logging, metrics, and alerts, enabling proactive detection and resolution of production issues."
+      "Maintained and enhanced Node.js and Express.js microservices supporting enrollment, billing, messaging, and analytics modules, serving 3,000+ parents and staff with high availability.",
+      "Developed reusable UI components and shared state services with Angular component architecture and lazy loading, reducing feature delivery time by 20%.",
+      "Optimized front end performance and API consumption through client-side caching, pagination, and debounced  requests, decreasing redundant network calls and improving responsiveness.",
+      "Implemented CI/CD pipelines with GitHub Actions, Docker, AWS ECR and AWS ECS (blue-green deployments) reducing deployment time by 25% and improving release reliability."
     ],
     technologies: ['Angular', 'Node.js', 'PostgreSQL', 'Express.js', 'RxJS', 'jest', 'Postman', 'GitHub Actions', 'Docker', 'AWS (ECR, ECS, CloudWatch)']
   },
@@ -52,9 +52,9 @@ export const timelineData: TimelineItem[] = [
     duration: 'May 2024 – Jul 2024',
     current: false,
     description: [
-      'Built an IU-branded WordPress block theme using PHP, HTML, JavaScript and Rivet 2 design system, improving UI/UX consistency across 500+ university blogs.',
-      'Ensured 100% WCAG 2.1 AA accessibility compliance using Siteimprove audits, optimizing templates and MySQL data handling.',
-      'Documented user guidelines, development process, and code structure, improving update cycles and onboarding efficiency for future developers.'
+      "Delivered an IU-branded WordPress block theme using PHP, HTML, JavaScript and Rivet 2 design system, standardizing UI/UX across 500+ university blogs.",
+      "Implemented full WCAG 2.1 AA accessibility compliance by auditing templates with Siteimprove and applying semantic HTML, and ARIA attributes, achieving 100% compliance scores.",
+      "Created developer and user documentation outlining workflows, code structure, and update processes, reducing onboarding time and improving future maintenance efficiency"
     ],
     technologies: ['HTML', 'CSS', 'JavaScript', 'PHP', ' IU’s Rivet Design System (Rivet 2)', 'WordPress', 'Siteimprove', 'MySQL']
   },
@@ -67,32 +67,11 @@ export const timelineData: TimelineItem[] = [
     duration: 'Jan 2023 – May 2024',
     current: false,
     description: [
-      'Built and maintained RESTful microservices using Node.js, Express, and PostgreSQL for an e-learning platform serving 5k+ active users, following Agile methodologies to ensure on-time delivery.',
-      'Optimized PostgreSQL performance through schema normalization, indexing strategies, and Redis caching, reducing query latency by 40% and improving overall API throughput.',
-      'Followed TDD practices to write unit and integration tests using Jest validating service logic, database interactions, and REST API contracts to reduce production defects.',
-      'Developed a payment microservice integrating PayPal APIs, implementing webhook reconciliation, idempotent transaction handling, and validated scalability via JMeter load testing for 2000+ concurrent users.'
+      "Built and maintained a scalable full stack e-learning platform using React on the frontend and Node.js, Express, and PostgreSQL on the backend, supporting 5,000+ active users with reliable end-to-end workflows.",
+      "Designed reusable and scalable React component architectures using Hooks, Context API, and modern UI patterns with real-time backend integrations, enabling data-driven dashboards and seamless payment workflows.",
+      "Optimized PostgreSQL performance through schema normalization, indexing strategies, and Redis caching, reducing query latency by 40% and improving overall API throughput.",
+      "Developed a payment microservice integrating PayPal APIs with webhook reconciliation, idempotent transaction handling, and validated scalability via JMeter load testing for 2000+ concurrent users."
     ],
     technologies: ['React', 'Tailwind CSS', 'Node.js', 'Express.js', 'PostgreSQL', 'PayPal Gateway', 'Redis', 'Docker', 'JMeter', 'GitHub Actions']
   },
-  // {
-  //   id: 4,
-  //   year: '2023',
-  //   title: 'Software Developer Intern',
-  //   company: 'SrasysInc',
-  //   location: 'Bangalore, India',
-  //   duration: 'Jan 2023 – Jul 2023',
-  //   current: false,
-  //   description: [
-  //     'Developed Srasys Academy, a full-stack e-learning platform using React, Tailwind CSS, Node.js, Express.js and PostgreSQL achieving 40% faster page load time and enabling a seamless experience for over 500 users.',
-  //     'Integrated PayPal API for secure transactions, supporting 100+ error-free payments within first month.',
-  //     '/n',
-  //     'Architected MeReg Application, a school management system using Angular, Node.js and PostgreSQL for Greystone House School, supporting enrollment and payments for 300+ users.',
-  //     'Implemented a plan-based fee payment system and integrated Stripe for secure transactions, utilizing webhooks to update payment status in the database and automating due remainders for parents.',
-  //     'Designed and implemented a scalable database schema to support 500+ initial records with 20% projected annual growth and a role-based access control for admins, parents and staff.'
-  //   ],
-  //   technologies: ['Angular', 'Tailwind CSS', 'Node.js', 'Firebase', 'API']
-  // },
-
-
-
 ];
