81eca238ee1e0f31b17aff5ae09a6a9dac5d526e
maheswar09 <maheswargorantla9@gmail.com>
2026-03-11 12:01:20 -0400

    removed descr

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -140,13 +140,13 @@ const Hero: React.FC = () => {
             >
               Get in Touch
             </a>
-            <a
+            {/* <a
               href="https://drive.google.com/file/d/19oz1G6f-ShIFik6S9sxAlUdZwlbAeWNQ/view?usp=sharing"
               target="_blank"
               className="px-6 py-3 rounded-full bg-gray-200 text-lg dark:bg-gray-700 hover:bg-gray-300 dark:hover:bg-gray-600 font-medium transition-colors duration-300"
             >
               Download Resume
-            </a>
+            </a> */}
           </motion.div>
 
           <motion.div
